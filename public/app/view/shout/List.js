a0c798b651d1b7af9f47b04db0eb4367f4b4a600
Albert Arvesu <albertarvesu@gmail.com>
2012-07-28 21:54:58 +0800

    Use speech bubbles for shouts.

--- public/app/view/shout/List.js
+++ public/app/view/shout/List.js
@@ -5,7 +5,7 @@ Ext.define('Gotashout.view.shout.List', {
         store: 'Shouts',
 
         disableSelection: true,
-        itemCls: 'shoutItem',
+
         itemTpl: Ext.create('Ext.XTemplate',
             '<div class="shout">',
                 '<div class="text">{text}</div>',
@@ -15,8 +15,7 @@ Ext.define('Gotashout.view.shout.List', {
             '<div class="picture">',
                 '<img src="https://graph.facebook.com/{profileId}/picture?type=square">',
                 '<span class="mood">{name} is {mood}</span>',
-            '</div>'
-
+            '</div>',
             {
                 timeAgoInWords: function(date) {
                     try {
