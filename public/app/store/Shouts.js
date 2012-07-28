37105c433829b770bc661cb9847df3355fc62b8c
Albert Arvesu <albertarvesu@gmail.com>
2012-07-28 11:33:13 +0800

    Test list.

--- public/app/store/Shouts.js
+++ public/app/store/Shouts.js
@@ -1,13 +1,9 @@
 
 Ext.define('Gotashout.store.Shouts', {
     extend  : 'Ext.data.Store',
-    requires: [
-        'Gotashout.model.Shout'
-    ],
 
     config: {
         model: 'Gotashout.model.Shout',
-        autoLoad: true,
 
         proxy: {
             type: 'jsonp',
