3e6bdc31856f2f4395f940e3393b990c58ae8533
Albert Arvesu <albertarvesu@gmail.com>
2012-07-28 10:16:59 +0800

    Added debug messages.

--- public/app/controller/Shouts.js
+++ public/app/controller/Shouts.js
@@ -20,7 +20,7 @@ Ext.define('Gotashout.controller.Shouts', {
 
     init: function() {
         this.callParent();
-
+console.log("init", Ext.getStore('Shouts'));
         Ext.getStore('Shouts').on('load', this.onShoutsLoad);
     },
 
